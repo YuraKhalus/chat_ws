169f7ede95be10c9963d2e99a728bef4dae0330a
Yurii Khalus <xalysjyra1992@gmail.com>
2026-03-14 13:37:31 +0200

    base for online chat by webSocket

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+   console.log("Користувач під'єднався");
+
+   socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+   })
+
+   socket.on('disconnect', () => {
+      console.log("Користувач від'єднався");
+   })
+})
+
+server.listen(3000, () => {
+   console.log("Sever is running on localhost:3000");
+})
