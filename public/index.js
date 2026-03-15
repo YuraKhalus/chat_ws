5e59c8d49a44213183d1489affe4d643c238d679
Marko <tarasenko.marko2007@gmail.com>
2026-03-15 15:00:15 +0100

    зробив щоб вiдрiзнялися мої повiдомлення вiд iнших подовiмлень. I зробив адаптив

--- public/index.js
+++ public/index.js
@@ -22,3 +22,23 @@
 //     messages.appendChild(item);
 //     window.scrollTo(0, document.body.scrollHeight);
 // })
+
+
+
+const message_box = document.querySelectorAll('.message_box');
+message_box.forEach((message, i) => {
+    if (i !== message_box.length - 1) {
+        const nextMessage = message.nextElementSibling.classList.contains('my_message')
+        const classMessage = message.classList.contains('my_message')
+        if (classMessage && nextMessage) {
+            message.style.marginBottom = '6px' 
+        } else if(!classMessage && nextMessage) {
+            message.style.marginBottom = '20px' 
+        } else if(classMessage && !nextMessage) {
+            message.style.marginBottom = '16px' 
+        }
+    }
+    if (message.querySelector('.box_text .text').textContent.split(' ').length > 10) {
+        message.querySelector('img').style.alignSelf = 'start'
+    }
+})
