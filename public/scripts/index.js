340cb9e51fd27a78bba3e5225d789a2d9b15b6ae
Max <zmax1301@gmail.com>
2026-03-28 15:28:27 +0100

    first_steps_for_convert_function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,9 @@
+// import convertImg from "../utils/convert.js";
+
 const socket = io();
 const userData = {
-  id: Math.floor(Math.random() * 1000),
-  username: "", //зробив пустим юзернейм
+    id: Math.floor(Math.random() * 1000),
+    username: "", //зробив пустим юзернейм
 };
 
 //-----------------------------
@@ -19,43 +21,44 @@ const avatar_img = document.querySelector(".avatar_img_show");
 document.body.style.overflow = "hidden";
 
 userForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  userData.username = userNameInput.value ? userNameInput.value : "Aнонім"; //тут юзернейм заповнюю
-  socket.emit("user joined", userData.username);
-  document.body.style.overflow = "";
-  modal.classList.remove("active");
+    e.preventDefault();
+    userData.username = userNameInput.value ? userNameInput.value : "Aнонім"; //тут юзернейм заповнюю
+    socket.emit("user joined", userData.username);
+    document.body.style.overflow = "";
+    modal.classList.remove("active");
 });
 
-userImgInput.addEventListener("change", (e) => {
-  const currFiles = e.target.files;
+userImgInput.addEventListener("change", async (e) => {
+    const currFiles = e.target.files;
 
-  if (currFiles.length > 0) {
-    let src = URL.createObjectURL(currFiles[0]);
-    avatar_img.src = src;
-  }
+    if (currFiles.length > 0) {
+        let src = URL.createObjectURL(currFiles[0]);
+        avatar_img.src = src;
+        await convertImg(currFiles[0]);
+    }
 });
 
 const login_modal = document.querySelector(".login");
 const registration_modal = document.querySelector(".registration");
 const switch_button_registration = document.querySelector(
-  ".switch_button_registration",
+    ".switch_button_registration",
 );
 const switch_button_login = document.querySelector(".switch_button_login");
 
 userForm_login.addEventListener("submit", (e) => {
-  e.preventDefault();
-  userData.username = userNameInput_login.value
-    ? userNameInput_login.value
-    : "Aнонім"; //тут юзернейм заповнюю
-  socket.emit("user joined", userData.username);
-  document.body.style.overflow = "";
-  modal_login.classList.remove("active");
+    e.preventDefault();
+    userData.username = userNameInput_login.value
+        ? userNameInput_login.value
+        : "Aнонім"; //тут юзернейм заповнюю
+    socket.emit("user joined", userData.username);
+    document.body.style.overflow = "";
+    modal_login.classList.remove("active");
 });
 
 function switch_modal(e) {
-  e.preventDefault();
-  login_modal.classList.toggle("active");
-  registration_modal.classList.toggle("active");
+    e.preventDefault();
+    login_modal.classList.toggle("active");
+    registration_modal.classList.toggle("active");
 }
 
 switch_button_registration.addEventListener("click", (e) => switch_modal(e));
@@ -65,26 +68,26 @@ switch_button_login.addEventListener("click", (e) => switch_modal(e));
 
 const form = document.querySelector("#form");
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const text = new FormData(form).get("text");
-  if (text) {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    socket.emit("chat message", {
-      ...userData,
-      data: hours + ":" + minutes,
-      message: text,
-    });
-    form.reset();
-  }
+    e.preventDefault();
+    const text = new FormData(form).get("text");
+    if (text) {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        socket.emit("chat message", {
+            ...userData,
+            data: hours + ":" + minutes,
+            message: text,
+        });
+        form.reset();
+    }
 });
 
 socket.on("chat message", (msg) => {
-  const li = document.createElement("li");
-  li.classList.add("message_box");
-  li.setAttribute("data-id", msg.id);
-  li.innerHTML = `
+    const li = document.createElement("li");
+    li.classList.add("message_box");
+    li.setAttribute("data-id", msg.id);
+    li.innerHTML = `
         <img src="./img/man.jpg" alt="" style="align-self: ${msg.message.split(" ").length > 10 ? "start" : "center"};">
         <div class="box_text">
             <p class="name">${msg.username}</p>
@@ -92,18 +95,18 @@ socket.on("chat message", (msg) => {
             <p class="time">${msg.data}</p>
         </div>
     `;
-  document.querySelector(".main__container").appendChild(li);
-  window.scrollTo(0, document.body.scrollHeight);
-  addClass();
+    document.querySelector(".main__container").appendChild(li);
+    window.scrollTo(0, document.body.scrollHeight);
+    addClass();
 });
 
 function addClass() {
-  const dataAttribute = document.querySelectorAll("[data-id]");
-  dataAttribute.forEach((att) => {
-    if (+att.getAttribute("data-id") == userData.id) {
-      att.classList.add("my_message");
-    }
-  });
+    const dataAttribute = document.querySelectorAll("[data-id]");
+    dataAttribute.forEach((att) => {
+        if (+att.getAttribute("data-id") == userData.id) {
+            att.classList.add("my_message");
+        }
+    });
 }
 
 export default socket;
---
+++ public/utils/convert.js
@@ -0,0 +1,44 @@
+// function convertImg(file, maxHeight = 100, maxWidth = 100, quality = 0.7) {
+//     return new Promise((resolve, reject) => {
+//         const reader = new FileReader();
+//         reader.readAsDataURL(file);
+
+//         reader.onload() = (event) => {
+//             const img = new Image();
+//             img.src = event.target.result;
+
+//             img.onload = () => {
+//                 let width = img.width;
+//                 let height = img.height;
+
+//                 if (width > height) {
+//                     if (width > maxWidth) {
+//                         height = Math.round((height * maxWidth) / width);
+//                         width = Math.maxWidth;
+//                     }
+//                 } else {
+//                     if (height > maxHeight) {
+//                         width = Math.round((width * maxHeight) / height);
+//                         height = maxHeight;
+//                     }
+//                 }
+
+//                 const canvas = document.createElement("canvas");
+//                 canvas.width = width;
+//                 canvas.height = height;
+
+//                 const ctx = canvas.getContext("2d");
+//                 ctx.drawImage(img, 0, 0, width, height);
+
+//                 const base64String = canvas.toDataURL("image/jpeg", quality);
+
+//                 console.log(base64String);
+//                 resolve(base64String);
+//             };
+//             img.onerror = (error) => reject(error);
+//         };
+//         reader.onerror = (error) => reject(error);
+//     });
+// }
+
+// export default convertImg;
